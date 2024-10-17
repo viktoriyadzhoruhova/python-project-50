74bdfb169ca722c0be5d4d3dff055e6d5274616e
Viktoriya Dzhoruhova <viktodzho@gmail.com>
2024-10-17 15:37:06 +0500

    Step 2

--- gendiff/scripts.py
+++ gendiff/scripts.py
@@ -1,14 +1,14 @@
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference."
-    )
-    parser.add_argument('first_file', help='First configuration file')
-    parser.add_argument('second_file', help='Second configuration file')
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', type=str, help='First configuration file')
+    parser.add_argument('second_file', type=str, help='Second configuration file')
+    parser.add_argument('-f', '--format', help='set format of output', default='plain')
+
     args = parser.parse_args()
-    # На данном этапе не делаем ничего с файлами, просто выводим сообщение
-    print("gendiff is working")
+
+    print(f"Comparing {args.first_file} and {args.second_file} with {args.format} format.")
 
 if __name__ == '__main__':
     main()
